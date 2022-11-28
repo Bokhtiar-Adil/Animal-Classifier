7ba016316d1a3e0f31922bf34cb2371d85807537
Bokhtiar-Adil <prottoy1807028@stud.kuet.ac.bd>
2022-11-28 14:25:40 +0600

    Details UI done+Dummy string added

--- app/src/main/java/com/example/animalclassifierv3/AnimalDetails.java
+++ app/src/main/java/com/example/animalclassifierv3/AnimalDetails.java
@@ -5,14 +5,33 @@ import java.util.ArrayList;
 
 public class AnimalDetails {
     public static final String[] OPTIONS = new String[]{
+            "Name",
             "Alert",
             "Brief",
             "Scientific Name",
+            "Conservation Status",
             "Nature",
             "Habitat",
             "Prominent Food",
-            "Specialities",
-            "Miscellaneous",
+            "Description"
+    };
+
+    public static final String[] Dummy = new String[]{
+            "Tence fish",
+            "Venomous. Time limit for death: 50 minutes.",
+            "The tench or doctor fish (Tinca tinca) is a fresh- and brackish-water fish of the order Cypriniformes found throughout Eurasia from Western Europe including the British Isles east into Asia as far as the Ob and Yenisei Rivers.",
+            "Tinca Tinca",
+            "Least Concern",
+            "Timid",
+            "The tench is most often found in still waters with a clay or muddy substrate and abundant vegetation. This species is rare in clear waters across stony substrate, and is absent altogether from fast-flowing streams. It tolerates water with a low oxygen concentration, being found in waters where even the carp cannot survive.",
+            "Tench feed mostly at night with a preference for animals, such as chironomids, on the bottom of eutrophic waters and snails and pea clams in well-vegetated waters.",
+            "Tench have a stocky, carp-like shape and olive-green skin, darker above and almost golden below. The tail fin is square in shape. The other fins are distinctly rounded in shape. The mouth is rather narrow and provided at each corner with a very small barbel. Maximum size is 70cm, though most specimens are much smaller. A record fish caught in 2001 in England had a weight of 15 lb 3 oz (6.89 kg). The eyes are small and red-orange in colour. Sexual dimorphism is absent or weak, limited to the adult females having a more convex ventral profile when compared with males.\n" +
+                    "\n" +
+                    "Males may also possess a very thick and flattened outer ray to the ventral fins. Males are generally smaller than females, but can be recognised by having more curved lower fins and noticeable muscles around the base of the fins generally absent in female.\n" +
+                    "\n" +
+                    "The tench has very small scales, which are deeply embedded in a thick skin, making it as slippery as an eel. Folklore has it that this slime cured any sick fish that rubbed against it, and from this belief arose the name doctor fish."
+
+
     };
 
     public static String[][] DETAILS = new String[][]{
