1068a051eaf1694df110a5738e8575b15eded514
Bokhtiar-Adil <prottoy1807028@stud.kuet.ac.bd>
2022-12-06 00:36:52 +0600

    WebViewActivity added+ResultActivity UI revisited

--- app/src/main/java/com/example/animalclassifierv3/AnimalDetails.java
+++ app/src/main/java/com/example/animalclassifierv3/AnimalDetails.java
@@ -15,9 +15,9 @@ public class AnimalDetails {
 
     public static final String[] Dummy = new String[]{
             "0",
-            "Tence fish",
-            "Venomous. Time limit for death: 50 minutes.",
-            "Tinca Tinca",
+            "Name of the animal",
+            "False",
+            "Tinca tinca",
             "Least Concern",
             "The tench or doctor fish (Tinca tinca) is a fresh- and brackish-water fish of the order Cypriniformes found throughout " +
                     "Eurasia from Western Europe including the British Isles east into Asia as far as the Ob and Yenisei Rivers.",
@@ -164,6 +164,27 @@ public class AnimalDetails {
                             "\n" +
                             "Open coniferous or birch woodland is favoured for breeding."
             },
+            {
+                "291",
+                    "Lion",
+                    "False",
+                    "Panthera leo",
+                    "Vulnerable",
+                    "The lion (Panthera leo) is a large cat of the genus Panthera native to Africa and India. It has a muscular, broad-chested body, short, " +
+                            "rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions are larger than females " +
+                            "and have a prominent mane. It is a social species, forming groups called prides. A lion's pride consists of a few adult males, " +
+                            "related females, and cubs. Groups of female lions usually hunt together, preying mostly on large ungulates. " +
+                            "The lion is an apex and keystone predator; although some lions scavenge when opportunities occur and have been known to hunt humans, the species typically does not actively seek out and prey on humans.\n" +
+                            "The lion inhabits grasslands, savannas and shrublands. It is usually more diurnal than other wild cats, but when persecuted, it adapts to being active at night and at twilight. During the Neolithic period, the lion ranged throughout Africa and Eurasia from Southeast Europe to India, " +
+                            "but it has been reduced to fragmented populations in sub-Saharan Africa and one population in western India. It has been listed as Vulnerable on the IUCN Red List since 1996 because populations in African countries have declined by about 43% since the early 1990s. " +
+                            "Lion populations are untenable outside designated protected areas. " +
+                            "Although the cause of the decline is not fully understood, habitat loss and conflicts with humans are the greatest causes for concern.\n" +
+                            "One of the most widely recognised animal symbols in human culture, the lion has been extensively depicted in sculptures and paintings, " +
+                            "on national flags, and in contemporary films and literature. Lions have been kept in menageries since the time of the Roman Empire and have been " +
+                            "a key species sought for exhibition in zoological gardens across the world since the late 18th century. Cultural depictions of lions w" +
+                            "ere prominent in Ancient Egypt, and depictions have occurred in virtually all ancient and medieval cultures in the lion's " +
+                            "historic and current range. "
+            }
 
     };
 
